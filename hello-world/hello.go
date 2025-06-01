619d3b5d79839f1345ca52141048ff34d1bf345c
suchao <822075222@qq.com>
2025-06-01 15:39:19 +0800

    feat: hello完成多语言输入与测试

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,5 +1,33 @@
 package hello_world
 
-func Hello() string {
-	return "hello, world"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "hello, "
+	spanishHelloPrefix = "hola, "
+	frenchHelloPrefix  = "bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) string {
+	prefix := ""
+
+	switch language {
+	case "":
+		prefix = englishHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,36 @@ package hello_world
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello()
-	excepted := "hello, world"
+	t.Run("say hello to people", func(t *testing.T) {
+		result := Hello("tom", "")
+		excepted := "hello, tom"
+
+		assertCorrectMessage(t, result, excepted)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		result := Hello("", "")
+		excepted := "hello, world"
+
+		assertCorrectMessage(t, result, excepted)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		result := Hello("Elodie", spanish)
+		want := "hola, Elodie"
+
+		assertCorrectMessage(t, result, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "bonjour, Lauren"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, result string, excepted string) {
+	t.Helper()
 
 	if result != excepted {
 		t.Errorf("excepted %s, but got %s", excepted, result)
