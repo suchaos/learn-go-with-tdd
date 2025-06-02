af36265619936fcb49abb46d68e369d054c56473
suchao <822075222@qq.com>
2025-06-02 11:31:33 +0800

    feat: map

---
+++ map/dctionary_test.go
@@ -0,0 +1,42 @@
+package _map
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		if err == nil {
+			t.Errorf("should have returned an error")
+		}
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+
+	if !errors.Is(got, want) {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertStrings(t testing.TB, got string, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
+}
---
+++ map/dictionary.go
@@ -0,0 +1,17 @@
+package _map
+
+import "errors"
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound = errors.New("could not find the word you were looking for")
+)
+
+func (dictionary Dictionary) Search(word string) (string, error) {
+	definition, ok := dictionary[word]
+	if ok {
+		return definition, nil
+	}
+	return "", ErrNotFound
+}
