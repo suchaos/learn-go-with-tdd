e1d0e7f1a0d1b10eb3980783a5056b744e4ddfeb
suchao <822075222@qq.com>
2025-06-05 22:31:09 +0800

    feat: select

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -17,21 +17,7 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		go func(urlInternal string) {
-			responseChan := make(chan bool, 1)
-
-			go func() {
-				responseChan <- wc(urlInternal)
-			}()
-
-			select {
-			case valid := <-responseChan:
-				resultChannel <- result{urlInternal, valid}
-			case <-time.After(20 * time.Millisecond):
-				fmt.Printf("Timeout for %s\n", url)
-				resultChannel <- result{urlInternal, false}
-			}
-		}(url)
+		go checkURLWithTimeout(wc, resultChannel, url)
 	}
 
 	for range len(urls) {
@@ -40,4 +26,20 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	}
 
 	return results
+}
+
+func checkURLWithTimeout(wc WebsiteChecker, resultChannel chan<- result, url string) {
+	responseChan := make(chan bool, 1)
+
+	go func() {
+		responseChan <- wc(url)
+	}()
+
+	select {
+	case valid := <-responseChan:
+		resultChannel <- result{url, valid}
+	case <-time.After(20 * time.Millisecond):
+		fmt.Printf("Timeout for %s\n", url)
+		resultChannel <- result{url, false}
+	}
 }
