bd22ccb712a5eebf738e65b00b9b8f6f88d0ad03
suchao <822075222@qq.com>
2025-06-09 21:16:16 +0800

    feat: template

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,7 +1,8 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"embed"
+	"html/template"
 	"io"
 )
 
@@ -10,25 +11,18 @@ type Post struct {
 	Tags                     []string
 }
 
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
 func Render(w io.Writer, post Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", post.Title, post.Description)
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprintf(w, "Tags: <ul>")
-	if err != nil {
-		return err
-	}
-
-	for _, tag := range post.Tags {
-		_, err := fmt.Fprintf(w, "<li>%s</li>", tag)
-		if err != nil {
-			return err
-		}
-	}
-
-	_, err = fmt.Fprintf(w, "</ul>")
+	err = templ.Execute(w, post)
 	if err != nil {
 		return err
 	}
